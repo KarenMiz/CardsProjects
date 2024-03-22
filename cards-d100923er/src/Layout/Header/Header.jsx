8a99fcb6aa51b088c7ab72390bce607cdb726479
KarenMiz <155911291+KarenMiz@users.noreply.github.com>
2024-03-22 21:40:33 +0200

    24/03/22

--- cards-d100923er/src/Layout/Header/Header.jsx
+++ cards-d100923er/src/Layout/Header/Header.jsx
@@ -4,6 +4,6 @@ import React from 'react'
 export default function Header() {
   return (
     <>
-    <Typography>NJ</Typography></>
+    <Typography></Typography></>
   )
 }
--- cards-d100923er/src/Routes/Router.jsx
+++ cards-d100923er/src/Routes/Router.jsx
@@ -3,6 +3,8 @@ import ROUTES from './routesModel'
 import CardPage from '../Cards/Pages/CardPage'
 import AboutPage from '../Pages/AboutPage'
 import { Route, Routes } from 'react-router-dom'
+import ErrorPage from '../Pages/ErrorPage'
+
 export default function Router() {
   return (
     <div>
@@ -10,7 +12,8 @@ export default function Router() {
       <Route path={ROUTES.ROOT} element={<CardPage />} />
       <Route path={ROUTES.CARDS} element={<CardPage />} />
       <Route path={ROUTES.ABOUT} element={<AboutPage/>} />
+      <Route path='*' element={<ErrorPage/>} />
     </Routes>
     </div>
-  )
+  );
 }
