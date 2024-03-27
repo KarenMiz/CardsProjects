8a02dfcc309cf6c594bd79fc93d69a5c24e5310e
KarenMiz <155911291+KarenMiz@users.noreply.github.com>
2024-03-27 19:07:12 +0200

    24/03/27

--- cards-d100923er/src/sandBox/LifeCycle.jsx
+++ cards-d100923er/src/sandBox/LifeCycle.jsx
@@ -1,7 +1,12 @@
 import React, { useEffect } from 'react'
 
 export default function LifeCycle() {
-  useEffect(() => {}, [])
+  useEffect(() => {
+    console.log("the component has mount"); 
+  return () => {
+    console.log("the component ha UNmount");};
+
+}, [])
 
 
   return (
